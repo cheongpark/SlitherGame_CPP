912e2776ddb594bd09b6a63daef3cfb08154c8f0
Cheongpark(청박) <pkdpms0328@gmail.com>
2021-07-31 23:39:46 +0900

    Key Input Test

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -3,9 +3,36 @@
 #include <conio.h>
 #include <string> //∑©≈∑ Ω√Ω∫≈€
 
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
 using namespace std;
 
 int main()
 {
-
+	int key;
+	while (1)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+			switch (key)
+			{
+			case UP:
+				cout << "UP" << endl;
+				break;
+			case DOWN:
+				cout << "DOWN" << endl;
+				break; 
+			case LEFT:
+				cout << "LEFT" << endl;
+				break; 
+			case RIGHT:
+				cout << "RIGHT" << endl;
+				break;
+			}
+		}
+	}
 }
